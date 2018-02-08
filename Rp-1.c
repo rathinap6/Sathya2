ceb4c1e06530936494418380cdf13155d1f2d7fb
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-02-08 16:09:11 +0530

    Create Rp-1.c

---
+++ Rp-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+   int n,i,a=1,b;
+   printf("Enter the number:");
+   scanf("%d",&n);
+   for(i=1;i<=5;i++)
+   {
+   b=n*i;
+   printf("%d\n",b);
+   }
+   
+}
+
+
+
